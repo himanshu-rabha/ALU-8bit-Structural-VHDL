6ed04800bc2612735108222593f9d62401b6a0c2
Himanshu Rabha <himanshurabhadesign@gmail.com>
2026-06-07 02:41:06 +0530

    Update HalfAdder.vhd

--- src/HalfAdder.vhd
+++ src/HalfAdder.vhd
@@ -1,1 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:51:43 05/29/2026 
+-- Design Name: 
+-- Module Name:    Halfadder - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Halfadder is
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           Sum : out  STD_LOGIC;
+           Carry : out  STD_LOGIC);
+end Halfadder;
+
+architecture Behavioral of Halfadder is
+
+begin
+		Sum <=A xor B;
+		Carry <=A and B;
+
+end Behavioral;
+
 
